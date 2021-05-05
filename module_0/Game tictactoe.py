3232bdaf79279993dfbef74b4c77c7ee9bb46478
LaysanMart <cloud_nine-@hotmail.com>
2021-05-05 10:16:18 +0300

    komments added

--- module_0/Game tictactoe.py
+++ module_0/Game tictactoe.py
@@ -5,12 +5,14 @@ print("Программа определит, кто выйграл, удачи!
 field = list(range(1,10))
 
 def show_field(field):
+      '''Игровое поле'''
       for i in range(3):
           print("|", field[0+i*3], "|", field[1+i*3], "|", field[2+i*3], "|")
           print("-" * 14)
 
 
 def player_entry(player_token):
+   """Проверка введенного пользователем значения"""
    while True:
       answer = int(input("Очередь хода " + player_token))
       if answer < 1 or answer > 9:
@@ -24,6 +26,7 @@ def player_entry(player_token):
 
 
 def check_winning(field):
+   """Проверка на выйгрышную комбинацию"""
    variants_of_win = ((0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6))
    for place in variants_of_win:
        if field[place[0]] == field[place[1]] == field[place[2]]:
@@ -37,6 +40,7 @@ def check_winning(field):
 
 
 def game_process(field):
+    """Игровой процесс"""
     count = 0
     while True:
         show_field(field)
